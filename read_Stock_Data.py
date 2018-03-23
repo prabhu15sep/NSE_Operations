9738ba633454f482353579fd7b1bd8f2b7cd1059
prabhu15sep <prabhu15sep@live.in>
2018-03-23 16:49:49 +0530

    Reads dat fiels of 5 NSE
    This reverts commit bea8b49543b220d787913ed1156f74d1ad019cd0.

--- read_Stock_Data.py
+++ read_Stock_Data.py
@@ -1,27 +1,28 @@
+
 """Reads the Stocks symbols and return Dataframe"""
 
 import os
 import pandas as pd
 
-def list_to_path(list_name, base_dir="/Volumes/2/PyD/"):    #"C:\\Users\\ppadayac\\"
+def list_to_path(list_name, base_dir="D:\\ppadayac\\PraChin\\db"):    #"D:\\ppadayac\\PraChin\\db"  "/Volumes/2/PyD/"
     """Return CSV file p  ath given ticker symbol."""
     return os.path.join(base_dir, "{}.csv".format(str(list_name)))
     
 def get_list(list_name,ind_key):
     df = pd.read_csv(list_to_path(list_name), index_col=ind_key,
-    parse_dates=True, usecols=['Symbol', 'Series','ISIN Code'], na_values=['nan'])
+            parse_dates=True, usecols=['Symbol', 'Series','ISIN Code'], na_values=['nan'])
     return df
 
-def get_data(symbol,start_date,days):
+def get_data(symbols,end_date,days):
     """Read stock data (adjusted close) for given symbols from CSV files."""
-    dates = pd.date_range(start=start_date, periods=20)
+    dates = pd.date_range(end=end_date, periods=90)
     df = pd.DataFrame(index=dates)
-    
-    df_temp = pd.read_csv(list_to_path(symbol), index_col='Date',
-            parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
-
-    df = df.join(df_temp)
-
-    return df
+    for symbol in symbols:
+        df_temp = pd.read_csv(list_to_path(symbol), index_col='Date',
+                parse_dates=True, usecols=['Date', 'Close Price'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Close Price': symbol})
+        df = df.join(df_temp)
+        df = df.dropna(subset=[symbol])
+    return df[0:days]
 
     
