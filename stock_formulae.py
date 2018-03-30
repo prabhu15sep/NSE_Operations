be42dfdb04d2600d7e794158cec6fa1154ee42dc
prabhu15sep <prabhu15sep@live.in>
2018-03-30 16:59:55 +0530

    Added Criterias

--- stock_formulae.py
+++ stock_formulae.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 
 def stockAvg(df_Stock,df_result_set):
-    df_temp = pd.DataFrame(df_Stock.mean().round(2))    #Calculating Mean of all Stocks
+    df_temp = pd.DataFrame(df_Stock.mean().round(3))    #Calculating Mean of all Stocks
     df_temp.columns =  ['Mean']                         #Naming the column as 'Mean'
     return df_result_set.join(df_temp)                  #Joining Mean data to the given set
 
 def stockRet(df_Stock,df_result_set,day):
     df_temp =(df_Stock/df_Stock.shift(day)) -1                       #Calculating Return of all Stocks
+    df_temp = df_temp.round(3)
     df_temp = (df_temp[-1:]).T                                       #Transpose
     df_temp.columns =  ['Return']                                    #Naming the column as 'Return'
     return df_result_set.join(df_temp)                               #Joining Mean data to the given set
